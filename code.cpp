8ca21aa09683b15b0d2542dd711a7ba31d16453f
jnnnn1999 <jnnnn1999@gmail.com>
2019-04-02 13:08:44 +0700

    welcome

---
+++ code.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+void greentext(){SetConsoleTextAttribute(hConsole, 2);  };
+void whitetext(){SetConsoleTextAttribute(hConsole, 15);  };
+void redtext (){SetConsoleTextAttribute(hConsole,  4);  };
+void bluetext (){SetConsoleTextAttribute(hConsole, 11);  };
+void yellowtext (){SetConsoleTextAttribute(hConsole, 14);  };
+void darktext (){SetConsoleTextAttribute(hConsole, 8);  };
+
+
+void welcome(){
+    bluetext();
+    cout << "\n\n\n\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+    cout <<       "\t\t\t@  ***********************************  @\n";
+    cout <<       "\t\t\t@  *                                 *  @\n";
+    cout <<       "\t\t\t@  *           WELCOME to            *  @\n";
+    cout <<       "\t\t\t@  *         La-lune-hotel           *  @\n";
+    cout <<       "\t\t\t@  *                                 *  @\n";
+    cout <<       "\t\t\t@  ***********************************  @\n";
+    cout <<       "\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+    whitetext();
+    cout <<       "\n\n\t\t\t\t>";
+    cout << "   Main menu \n";
+    greentext();
+    cout << "\t\t      press enter in order to approach Main menu... ";
+    cin.get(); 
+}
+int main(){
+    welcome();
+     while (MenuLoop == true){mainMenu();};      //Main menu loop
+    return 0;
+}
+
