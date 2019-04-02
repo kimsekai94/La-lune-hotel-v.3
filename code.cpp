49b180785b863d771b1bb7eaffdba9311ec5b471
unknown <nsrichuwong@gmail.com>
2019-04-02 13:34:23 +0700

    A++

--- code.cpp
+++ code.cpp
@@ -17,48 +17,47 @@ void darktext (){SetConsoleTextAttribute(hConsole, 8);  };
 
 
 bool MenuLoop = true;
-string name ("*****");    
-string surname ("*****");
-int address1 (00);
-string address2 ("*****");
-string address3 ("*****");
-string address4 ("*****");
-string postcode ("*****");
-string tel ("00");
+string name("*****");    
+string surname("*****");
+int address1(00);
+string address2("*****");
+string address3("*****");
+string address4("*****");
+string postcode("*****");
+string tel("00");
 int creditcardSelection;
 string creditcardType("*****");
 string creditcard("****"); 
 
 void resetMemberDetails(){
-     name =     "*****";
-     surname =  "*****";
-     address1 = 00;
-     address2 = "*****";
-     address3 = "*****";
-     address4 = "*****";
-     postcode = "*****";
-     tel = "00";
-     creditcard = "****";
-     creditcardType   = "*****";
+    name = "*****";
+    surname = "*****";
+    address1 = 00;
+    address2 = "*****";
+    address3 = "*****";
+    address4 = "*****";
+    postcode = "*****";
+    tel = "00";
+    creditcard = "****";
+    creditcardType = "*****";
  
 }
 void MemberForm(){
- 
     system ("cls");
     bluetext();
-    cout << "\t\t\t      Member Details\n";
+    cout << "\n\t\t\t      Member Details\n";
     whitetext();
     cout << "\t________________________________________________________________\n\n";
     cout << "\tMember Form   \t";
-    cout << "\t  Member: ";
-    if (name != "*****"){greentext();};   // If the field is not the default
-    cout << name << " ";                  // value use green text, then
-    whitetext();                          // revert back to white text.
+    cout << "\t  Member : ";
+    if (name != "*****"){greentext();};   
+    cout << name << " ";                  
+    whitetext();                          
     if (surname != "*****"){greentext();};
     cout << surname << "\n";
     whitetext();
     cout << "\t\t\t";
-    cout << "\t Address: ";
+    cout << "\t Address : ";
     if (address1 != 00){greentext();};
     cout << address1 << " ";
     whitetext();
@@ -78,17 +77,17 @@ void MemberForm(){
     if (postcode != "*****"){greentext();};
     cout << postcode << "\n";
     whitetext();
-    cout << "\t\t\t   Telephone: ";
+    cout << "\t\t\t   Telephone : ";
     if (tel != "00"){greentext();};
     cout << tel;
     whitetext();
     cout << "\n";
     cout << "\t________________________________________________________________\n\n";
-    cout << "\t\t           Credit card: ";
+    cout << "\t\t           Credit card : ";
     if (creditcard != "****"){greentext();};
     cout << creditcard;
     whitetext();
-    cout << "\n\t\t         Credit card Type: ";
+    cout << "\n\t\t         Credit card Type : ";
     if(creditcard == "No"){
         greentext();
         creditcardType = "No";
@@ -99,12 +98,104 @@ void MemberForm(){
     cout << "\n\t________________________________________________________________\n\n";
     cout << "\n";
 };
- 
-// Save function for saving member details to a file
+
 void savefile(){
     string saveFileName;
     stringstream out;
     out << name << " " << surname;
+    saveFileName = out.str();
+    ofstream myfile;                                  
+    myfile.open (saveFileName.c_str(), ios::out);
+    myfile << name << "\n";
+    myfile << surname << "\n";
+    myfile << address1 << "\n";
+    myfile << address2 << "\n";
+    myfile << address3 << "\n";
+    myfile << address4 << "\n";
+    myfile << postcode << "\n";
+    myfile << tel<< "\n";
+    myfile << creditcard << "\n";
+    myfile << creditcardType << "\n";
+    myfile.close();
+    MemberForm();
+    greentext();
+    cout << "\tDetails have been saved\n\n\t";
+    whitetext();
+    system ("pause");
+    }
+ 
+
+void memberDetails(){
+ 
+    char saveYN;
+ 
+    MemberForm();
+    cout << "\tPlease enter customers first name : ";
+    cin >> ws;
+    getline (cin, name);
+    MemberForm();
+    cout << "\tPlease enter customers surname : ";
+    cin >> surname;
+    MemberForm();
+    cout << "\tPlease enter customers house number : ";
+    cin >> address1;
+                while (cin.fail()||  address1 < 1)  
+    {cin.clear(); cin.ignore();
+    cout << "\n\tPlease enter a valid house number : "; cin >> address1;}
+    MemberForm();
+    cout << "\tPlease enter customers street : ";
+    cin >> ws;                        
+    getline (cin, address2);         
+    MemberForm();
+    cout << "\tPlease enter customer area : ";
+    getline (cin, address3);
+    MemberForm();
+    cout << "\tPlease enter customer city : ";
+    getline (cin, address4);
+    MemberForm();
+    cout << "\tPlease enter customer postcode : ";
+    cin >> ws;
+    getline (cin, postcode);
+    MemberForm();
+    cout << "\tPlease enter Telephone Number : ";
+    getline (cin, tel);
+    while (cin.fail()){ 
+        cin.clear();
+        cin.ignore();
+        cout << "\tPlease enter Telephone Number : ";
+        cin >> tel;
+    };
+    MemberForm();
+    cout << "\tCould you have credit card? <Yes/No> ";
+    cin >> creditcard;
+    MemberForm();
+    if(creditcard == "Yes"){
+        cout << "\tPlease enter Credit card Type:\n\t1 - GSB\n\t2 - KTB\n\t3 - SCB\n\t> : ";
+        cin >> creditcardSelection;
+        
+        switch (creditcardSelection){
+            case 1 :
+                creditcardType = "GSB";
+                break;
+            case 2 :
+                creditcardType = "KTB";
+                break;
+            case 3 :
+                creditcardType = "SCB";
+                break;
+        }
+    }   
+    
+    MemberForm();
+    
+    cout << "\tSave these details to a file? <Y/N> ";
+ 
+    //Confirmation before saving a file
+    cin >> saveYN;
+    if (saveYN == 'y' || saveYN == 'Y'){
+        savefile();
+    };
+};
     
 void createBooking(){}
 void billpayment(){}
@@ -112,8 +203,7 @@ void priceList(){}
 void help(){}
 void exit(){}
 void mainMenu(){
- 
-    // Private variable used for menu selection
+
     int menuchoice;
  
     system ("cls");
@@ -128,20 +218,17 @@ void mainMenu(){
     cout << "\t\t4 - bill payment\n\n";
     cout << "\t\t5 - Help\n\n";
     cout << "\t\t6 - Exit\n\n\n";
-    cout << "\t\t\tPlease choose an option: ";
+    cout << "\t\t\tPlease choose an option : ";
     cin >> menuchoice;
- 
-    //Prevents a character entry from crashing the program
+
     while (cin.fail())
         {cin.clear();
         cin.ignore();
-        cout << "\n\t\tPlease enter a valid number:"; cin >> menuchoice;
+        cout << "\n\t\tPlease enter a valid number :"; cin >> menuchoice;
     };
  
-    // Switch statement for menu selection
     switch (menuchoice){
  
-        // Enter new member function
         case 1 :
             memberDetails();
             break;
@@ -190,7 +277,7 @@ void welcome(){
 }
 int main(){
     welcome();
-     while (MenuLoop == true){mainMenu();};      //Main menu loop
+    while (MenuLoop == true){mainMenu();};     
     return 0;
 }
 
