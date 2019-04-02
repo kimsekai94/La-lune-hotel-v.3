886b1daec7a52c0e01619e5aa845bc3320bbe0b3
jnnnn1999 <jnnnn1999@gmail.com>
2019-04-02 13:11:14 +0700

    mainmenu

--- code.cpp
+++ code.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 #include <windows.h>
 
 using namespace std;
@@ -11,6 +12,70 @@ void bluetext (){SetConsoleTextAttribute(hConsole, 11);  };
 void yellowtext (){SetConsoleTextAttribute(hConsole, 14);  };
 void darktext (){SetConsoleTextAttribute(hConsole, 8);  };
 
+void createBooking(){}
+void billpayment(){}
+void priceList(){}
+void help(){}
+void exit(){}
+void mainMenu(){
+ 
+    // Private variable used for menu selection
+    int menuchoice;
+ 
+    system ("cls");
+ 
+    resetMemberDetails();
+    bluetext();
+    cout << "\t\t\t\tMain Menu\n\n";
+    whitetext();
+    cout << "\t\t1 - Add a new member\n\n";
+    cout << "\t\t2 - View price list\n\n";
+    cout << "\t\t3 - Create a new booking\n\n";
+    cout << "\t\t4 - bill payment\n\n";
+    cout << "\t\t5 - Help\n\n";
+    cout << "\t\t6 - Exit\n\n\n";
+    cout << "\t\t\tPlease choose an option: ";
+    cin >> menuchoice;
+ 
+    //Prevents a character entry from crashing the program
+    while (cin.fail())
+        {cin.clear();
+        cin.ignore();
+        cout << "\n\t\tPlease enter a valid number:"; cin >> menuchoice;
+    };
+ 
+    // Switch statement for menu selection
+    switch (menuchoice){
+ 
+        // Enter new member function
+        case 1 :
+            memberDetails();
+            break;
+        case 2 :
+            createBooking();
+            break;
+ 
+        case 3 :
+            billpayment();
+            break;
+ 
+        case 4 :
+            priceList();
+            break;
+ 
+        case 5 :
+            help();
+            break;
+ 
+        case 6 :
+            exit();
+            break;
+ 
+        default:
+            cout << "\n\tPlease enter a correct menu choice\n\n\t";
+            system ("pause");
+    };
+};
 
 void welcome(){
     bluetext();
